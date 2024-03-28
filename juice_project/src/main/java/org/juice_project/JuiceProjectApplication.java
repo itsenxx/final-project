2e17781e1b38177e7dccd2800c0ac35615dd7a34
hanry <itsenxx99@gmail.com>
2024-03-28 17:15:33 +0900

    2024-03-28 first

---
+++ juice_project/src/main/java/org/juice_project/JuiceProjectApplication.java
@@ -0,0 +1,13 @@
+package org.juice_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuiceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuiceProjectApplication.class, args);
+	}
+
+}
---
+++ juice_project/src/test/java/org/juice_project/demo/JuiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.juice_project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
